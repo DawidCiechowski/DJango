777a743afb99f0531d14a50da5d3d08f97ca9fec
DawidCiechowski <dawid.ciechowski@gmail.com>
2019-04-09 03:40:45 +0100

    Another basic set of comments

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Product, Offer
 
+#List all the fields to display
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('code', 'discount')
-    
+
 #Commit all the changes
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Offer, OfferAdmin)
